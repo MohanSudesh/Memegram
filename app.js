16c7f8d942e18a4a817a9bb9c0750b22bba148d0
Sudesh <ce18b025@smail.iitm.ac.in>
2021-09-29 13:15:59 +0530

    removing cors

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const PORT = process.env.PORT || 5000;
 
 require("./models/user");
 require("./models/post");
-app.use(cors());
 app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
